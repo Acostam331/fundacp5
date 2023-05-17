f65f7c5010cf97658582539090f8cc5fb57b009e
acostam331 <acostam331@gmail.com>
2023-05-17 09:40:23 -0600

    hola

--- main.cpp
+++ main.cpp
@@ -3,8 +3,15 @@
 using namespace std;
 
 int main(){
+    int n1 =2;
+    int n2 =3;
     cout << "Hola mundo";
     cout << "Hola mundo";
     cout << "Hola xd";
+
+    if(n1 != n2){
+        cout << "True";
+    }
+
     return 0;
 }
